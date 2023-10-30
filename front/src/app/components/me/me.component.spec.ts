8388e67db80ee2df48744aae57a5f4b6abb55b5d
Pertoldi <antoine.pertoldi@gmail.com>
2023-10-30 15:28:59 +0100

    :white_check_mark: add test on front service

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -5,12 +5,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { SessionService } from 'src/app/services/session.service';
 
 import { MeComponent } from './me.component';
 import { of } from 'rxjs';
 import { User } from 'src/app/interfaces/user.interface';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 
 describe('MeComponent', () => {
@@ -38,9 +40,9 @@ describe('MeComponent', () => {
 
   const mockUser = {
     id: 1,
-    email: 'test@test.com',
-    lastName: 'testln',
-    firstName: 'testfn',
+    email: 'jean@dupont.com',
+    lastName: 'Dupont',
+    firstName: 'Jean',
     admin: false,
     password: 'password123',
     createdAt: new Date(),
@@ -66,11 +68,15 @@ describe('MeComponent', () => {
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
-        MatInputModule
+        MatInputModule,
       ],
-      providers: [{ provide: SessionService, useValue: mockSessionService }],
-    })
-      .compileComponents();
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: UserService, useValue: mockUserService },
+        { provide: Router, useValue: mockRouter },
+        { provide: MatSnackBar, useValue: mockMatSnackBar },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MeComponent);
     component = fixture.componentInstance;
--- front/src/app/features/sessions/services/session-api.service.spec.ts
+++ front/src/app/features/sessions/services/session-api.service.spec.ts
@@ -3,20 +3,64 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionApiService } from './session-api.service';
+import { SessionService } from 'src/app/services/session.service';
 
 describe('SessionsService', () => {
-  let service: SessionApiService;
+  let service: SessionService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
-    });
-    service = TestBed.inject(SessionApiService);
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SessionService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  test('should initialize the service with parameters isLogged to false and sessionInformation to undefined', () => {
+    expect(service.isLogged).toBe(false);
+    expect(service.sessionInformation).toBeUndefined();
+  });
+
+  it('should login an user', () => {
+    const user = {
+      token: 'test_token',
+      type: 'test_user_type',
+      id: 1,
+      username: 'testuser',
+      firstName: 'Toto',
+      lastName: 'Test',
+      admin: false,
+    };
+
+    service.logIn(user);
+
+    expect(service.isLogged).toBe(true);
+    expect(service.sessionInformation).toEqual(user);
+  });
+
+  it('should log out an user by setting user to undefined and isLogged to false', () => {
+    const user = {
+      token: 'test_token',
+      type: 'test_user_type',
+      id: 1,
+      username: 'testuser',
+      firstName: 'Toto',
+      lastName: 'Test',
+      admin: false,
+    };
+
+    service.sessionInformation = user;
+    service.isLogged = true;
+
+    service.logOut();
+
+    expect(service.isLogged).toBe(false);
+    expect(service.sessionInformation).toBeUndefined();
+  });
+
+  it('should return an observable for isLogged', () => {
+    const isLogged = service.$isLogged();
+    expect(isLogged).toBeTruthy();
   });
 });
