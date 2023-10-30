c857f2b5502fdcee45e03c7737ebeba7910f0679
Pertoldi <antoine.pertoldi@gmail.com>
2023-10-30 15:28:14 +0100

    :white_check_mark: Add test on back service

---
+++ back/src/test/java/services/TeacherServiceTest.java
@@ -0,0 +1,84 @@
+package services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.repository.TeacherRepository;
+import com.openclassrooms.starterjwt.services.TeacherService;
+
+public class TeacherServiceTest {
+  @Mock
+  private TeacherRepository teacherRepository;
+
+  @InjectMocks
+  private TeacherService teacherService;
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void testFindAllTeachers() {
+    // GIVEN
+    // Create a list of mock teachers for the findAll method
+    List<Teacher> mockTeachers = new ArrayList<>();
+    mockTeachers.add(new Teacher());
+    mockTeachers.add(new Teacher());
+
+    // Configure the mock to return the list of mock teachers when findAll is called
+    when(teacherRepository.findAll()).thenReturn(mockTeachers);
+
+    // WHEN
+    List<Teacher> retrievedTeachers = teacherService.findAll();
+
+    // THEN
+    // Verify that the findAll method returned the expected list of teachers
+    assertThat(retrievedTeachers).isEqualTo(mockTeachers);
+  }
+
+  @Test
+  public void testGetTeacherById() {
+    // GIVEN
+    Long teacherId = 1L;
+    Teacher expectedTeacher = new Teacher();
+    expectedTeacher.setId(teacherId);
+
+    when(teacherRepository.findById(teacherId)).thenReturn(Optional.of(expectedTeacher));
+
+    // WHEN
+    // Call the findById method with the specific ID
+    Teacher retrivedTeacher = teacherService.findById(teacherId);
+
+    // THEN
+    assertThat(retrivedTeacher).isEqualTo(expectedTeacher);
+  }
+
+  @Test
+  public void testGetTeacherById_TeacherNotFound() {
+    // GIVEN
+    Long teacherId = 1L;
+    Teacher expectedTeacher = new Teacher();
+    expectedTeacher.setId(teacherId);
+
+    when(teacherRepository.findById(teacherId)).thenReturn(Optional.of(expectedTeacher));
+
+    // WHEN
+    // Call the findById method with the specific ID
+    Teacher retrivedTeacher = teacherService.findById(2L);
+
+    // THEN
+    assertThat(retrivedTeacher).isNotEqualTo(expectedTeacher);
+  }
+}
---
+++ back/src/test/java/services/UserServiceTest.java
@@ -0,0 +1,68 @@
+package services;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.services.UserService;
+
+public class UserServiceTest {
+
+  @Mock
+  private UserRepository userRepository;
+
+  @InjectMocks
+  private UserService userService;
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void testDeleteUser() {
+    // GIVEN
+    Long userIdToDelete = 1L;
+
+    // Configure the mock to simulate the behavior of userRepository.deleteById
+    doNothing().when(userRepository).deleteById(userIdToDelete);
+
+    // WHEN
+    // Call the delete method with the user ID to delete
+    userService.delete(userIdToDelete);
+
+    // THEN
+    // Verify that the userRepository.deleteById method was called with the user ID
+    // to delete
+    verify(userRepository).deleteById(userIdToDelete);
+  }
+
+  @Test
+  public void testFindUserById() {
+    // GIVEN
+    Long userId = 1L;
+    User expectedUser = new User();
+    expectedUser.setId(userId);
+
+    // Configure the mock to simulate the behavior of userRepository.findById
+    when(userRepository.findById(userId)).thenReturn(java.util.Optional.of(expectedUser));
+
+    // WHEN
+    // Call the findById method with the user ID
+    User retrievedUser = userService.findById(userId);
+
+    // THEN
+    // Verify that the user returned by the service is the expected user
+    assertThat(retrievedUser).isEqualTo(expectedUser);
+  }
+}
