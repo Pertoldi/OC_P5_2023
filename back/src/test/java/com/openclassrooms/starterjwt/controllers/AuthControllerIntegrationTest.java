e9b77349a9414dbd9f222bdb9b6c758450e30e0b
Pertoldi <antoine.pertoldi@gmail.com>
2023-12-11 19:08:29 +0100

    Use ObjectMapper (#8)

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerIntegrationTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerIntegrationTest.java
@@ -13,6 +13,9 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:rollback.sql")
@@ -21,15 +24,19 @@ public class AuthControllerIntegrationTest {
   @Autowired
   private MockMvc mockMvc;
 
+  ObjectMapper ObjMapper = new ObjectMapper();
+
   @Test
-  public void testAuthenticateUser_ValidLogin() throws Exception {
+  public void testAuthenticateUser() throws Exception {
     // Create a JSON request body with user information
-    String requestBody = "{\"email\":\"yoga@studio.com\",\"password\":\"test!1234\"}";
+    ObjectNode requestBody = ObjMapper.createObjectNode();
+    requestBody.put("email", "yoga@studio.com");
+    requestBody.put("password", "test!1234");
 
     // Perform an HTTP POST request to the /api/auth/register endpoint with the JSON
     MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/login")
         .contentType(MediaType.APPLICATION_JSON)
-        .content(requestBody))
+        .content(ObjMapper.writeValueAsString(requestBody)))
         .andReturn();
     assertThat(result.getResponse().getStatus()).isEqualTo(200);
   }
@@ -37,26 +44,34 @@ public class AuthControllerIntegrationTest {
   @Test
   public void testRegisterUser() throws Exception {
     // Create a JSON request body with user information
-    String requestBody = "{\"email\":\"test@example.com\",\"password\":\"password\",\"firstName\":\"Test\",\"lastName\":\"Example\"}";
+    ObjectNode requestBody = ObjMapper.createObjectNode();
+    requestBody.put("email", "test@example.com");
+    requestBody.put("password", "password");
+    requestBody.put("firstName", "Test");
+    requestBody.put("lastName", "Exemple");
 
     // Perform an HTTP POST request to the /api/auth/register endpoint with the JSON
     MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
         .contentType(MediaType.APPLICATION_JSON)
-        .content(requestBody))
+        .content(ObjMapper.writeValueAsString(requestBody)))
         .andExpect(status().isOk())
         .andReturn();
     assertThat(result.getResponse().getStatus()).isEqualTo(200);
   }
 
   @Test
-  public void testRegisterUser_EmailAlreadyTaken() throws Exception {
+  public void testRegisterUserEmailAlreadyTaken() throws Exception {
     // Create a JSON request body with an email that is already in the database
-    String requestBody = "{\"email\":\"yoga@studio.com\",\"password\":\"password\",\"firstName\":\"Test\",\"lastName\":\"Example\"}";
+    ObjectNode requestBody = ObjMapper.createObjectNode();
+    requestBody.put("email", "yoga@studio.com");
+    requestBody.put("password", "password");
+    requestBody.put("firstName", "Test");
+    requestBody.put("lastName", "Exemple");
 
     // Perform an HTTP POST request to the /api/auth/register endpoint with the JSON
     MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
         .contentType(MediaType.APPLICATION_JSON)
-        .content(requestBody))
+        .content(ObjMapper.writeValueAsString(requestBody)))
         .andExpect(MockMvcResultMatchers.status().isBadRequest()) // Expecting a bad request
         .andReturn();
 
